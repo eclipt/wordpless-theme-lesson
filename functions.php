b7e2aca01ee8bbe42c1d34179b10ce8b18014d21
chiekitahara <eculiptus@gmail.com>
2021-01-28 22:11:14 +0900

    ドロワーの見た目調整

--- functions.php
+++ functions.php
@@ -32,11 +32,11 @@ add_image_size( 'category-thumb', 300, 9999 );
 
 // スタイルシートのリンクを取得する関数、引数にpc-cssという名前、
     function sakura_theme_link(){
-        wp_enqueue_style('common-css', get_template_directory_uri() . '/css/style.css');
         wp_enqueue_style('sp-css', get_template_directory_uri() . '/css/style_sp.css', array(), '1.0.0', 'screen and ( max-width:768px )');
         wp_enqueue_style('pc-css', get_template_directory_uri() . '/css/style_pc.css', array(), '1.0.0', 'screen and ( min-width:769px )');
         wp_enqueue_style('drawer-css', "https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css" );
-
+        wp_enqueue_style('common-css', get_template_directory_uri() . '/css/style.css');
+        
         wp_enqueue_script('jquery', "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js");
         wp_enqueue_script('iscroll', "https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js");
         wp_enqueue_script('drawer-js', "https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/js/drawer.min.js");
